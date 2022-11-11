f0ef6fb8e7f50b3f3b2d28ec3fec9595a1ea4d60
yumi <mycaptain0771@gmail.com>
2022-11-11 18:19:14 +0900

    ok

---
+++ src/main/java/net/ict/springex/controller/TodoController.java
@@ -0,0 +1,27 @@
+package net.ict.springex.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/todo")
+@Log4j2
+public class TodoController {
+        @RequestMapping("/list")      //     최종경로는 /todo/list
+        public void list(){
+            log.info("todo list............");
+            }
+        @RequestMapping(value = "/register", method = RequestMethod.GET)
+        public void register(){
+            log.info("todo register...........");
+        }
+        @PostMapping("/register")
+        public void registerPost(){
+              log.info("POST todo register ...........");
+        }
+
+
+}
---
+++ src/main/webapp/WEB-INF/views/hello.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: ict05-00
+  Date: 2022-11-11
+  Time: 오후 4:22
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>hello.jsp</title>
+</head>
+<body>
+<h1>Hello  Spring MVC!!! </h1>
+</body>
+</html>
